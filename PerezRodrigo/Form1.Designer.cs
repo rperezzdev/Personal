76cc5eda24d627015f4dd8b35c278d150219f4da
rperezzdev <rperezzdev@gmail.com>
2025-12-18 16:51:57 -0300

    Add files via upload
    primer recup

---
+++ PerezRodrigo/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace PerezRodrigo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbPrecio = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.tbDescripcion = new System.Windows.Forms.TextBox();
+            this.tbId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnFiltrar = new System.Windows.Forms.Button();
+            this.btnDesfltrar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbDesde = new System.Windows.Forms.TextBox();
+            this.tbHasta = new System.Windows.Forms.TextBox();
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descripcionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.precioDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.recuperatorioDataSet = new PerezRodrigo.RecuperatorioDataSet();
+            this.productosTableAdapter = new PerezRodrigo.RecuperatorioDataSetTableAdapters.ProductosTableAdapter();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productosBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.recuperatorioDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbPrecio);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.btnLimpiar);
+            this.groupBox1.Controls.Add(this.btnEliminar);
+            this.groupBox1.Controls.Add(this.btnModificar);
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Controls.Add(this.tbDescripcion);
+            this.groupBox1.Controls.Add(this.tbId);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(483, 178);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos del producto";
+            // 
+            // tbPrecio
+            // 
+            this.tbPrecio.Location = new System.Drawing.Point(124, 109);
+            this.tbPrecio.Name = "tbPrecio";
+            this.tbPrecio.Size = new System.Drawing.Size(339, 20);
+            this.tbPrecio.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(46, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Precio";
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(372, 149);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 7;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(278, 149);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 6;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(182, 149);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 5;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(81, 149);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // tbDescripcion
+            // 
+            this.tbDescripcion.Location = new System.Drawing.Point(124, 65);
+            this.tbDescripcion.Name = "tbDescripcion";
+            this.tbDescripcion.Size = new System.Drawing.Size(339, 20);
+            this.tbDescripcion.TabIndex = 3;
+            // 
+            // tbId
+            // 
+            this.tbId.Location = new System.Drawing.Point(124, 16);
+            this.tbId.Name = "tbId";
+            this.tbId.Size = new System.Drawing.Size(339, 20);
+            this.tbId.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Descripcion";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnFiltrar);
+            this.groupBox2.Controls.Add(this.btnDesfltrar);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.tbDesde);
+            this.groupBox2.Controls.Add(this.tbHasta);
+            this.groupBox2.Location = new System.Drawing.Point(512, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(259, 138);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Filtrar por precio";
+            // 
+            // btnFiltrar
+            // 
+            this.btnFiltrar.Location = new System.Drawing.Point(46, 90);
+            this.btnFiltrar.Name = "btnFiltrar";
+            this.btnFiltrar.Size = new System.Drawing.Size(75, 23);
+            this.btnFiltrar.TabIndex = 8;
+            this.btnFiltrar.Text = "Filtrar";
+            this.btnFiltrar.UseVisualStyleBackColor = true;
+            this.btnFiltrar.Click += new System.EventHandler(this.btnFiltrar_Click);
+            // 
+            // btnDesfltrar
+            // 
+            this.btnDesfltrar.Location = new System.Drawing.Point(140, 90);
+            this.btnDesfltrar.Name = "btnDesfltrar";
+            this.btnDesfltrar.Size = new System.Drawing.Size(75, 23);
+            this.btnDesfltrar.TabIndex = 9;
+            this.btnDesfltrar.Text = "Desfiltrar";
+            this.btnDesfltrar.UseVisualStyleBackColor = true;
+            this.btnDesfltrar.Click += new System.EventHandler(this.btnDesfltrar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(137, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Hasta";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Desde";
+            // 
+            // tbDesde
+            // 
+            this.tbDesde.Location = new System.Drawing.Point(21, 39);
+            this.tbDesde.Name = "tbDesde";
+            this.tbDesde.Size = new System.Drawing.Size(100, 20);
+            this.tbDesde.TabIndex = 4;
+            // 
+            // tbHasta
+            // 
+            this.tbHasta.Location = new System.Drawing.Point(140, 39);
+            this.tbHasta.Name = "tbHasta";
+            this.tbHasta.Size = new System.Drawing.Size(100, 20);
+            this.tbHasta.TabIndex = 5;
+            // 
+            // dgvProductos
+            // 
+            this.dgvProductos.AutoGenerateColumns = false;
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.descripcionDataGridViewTextBoxColumn,
+            this.precioDataGridViewTextBoxColumn});
+            this.dgvProductos.DataSource = this.productosBindingSource;
+            this.dgvProductos.Location = new System.Drawing.Point(26, 196);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.Size = new System.Drawing.Size(745, 197);
+            this.dgvProductos.TabIndex = 2;
+            this.dgvProductos.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // descripcionDataGridViewTextBoxColumn
+            // 
+            this.descripcionDataGridViewTextBoxColumn.DataPropertyName = "Descripcion";
+            this.descripcionDataGridViewTextBoxColumn.HeaderText = "Descripcion";
+            this.descripcionDataGridViewTextBoxColumn.Name = "descripcionDataGridViewTextBoxColumn";
+            // 
+            // precioDataGridViewTextBoxColumn
+            // 
+            this.precioDataGridViewTextBoxColumn.DataPropertyName = "Precio";
+            this.precioDataGridViewTextBoxColumn.HeaderText = "Precio";
+            this.precioDataGridViewTextBoxColumn.Name = "precioDataGridViewTextBoxColumn";
+            // 
+            // productosBindingSource
+            // 
+            this.productosBindingSource.DataMember = "Productos";
+            this.productosBindingSource.DataSource = this.recuperatorioDataSet;
+            // 
+            // recuperatorioDataSet
+            // 
+            this.recuperatorioDataSet.DataSetName = "RecuperatorioDataSet";
+            this.recuperatorioDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // productosTableAdapter
+            // 
+            this.productosTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvProductos);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productosBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.recuperatorioDataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox tbDescripcion;
+        private System.Windows.Forms.TextBox tbId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnFiltrar;
+        private System.Windows.Forms.Button btnDesfltrar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbDesde;
+        private System.Windows.Forms.TextBox tbHasta;
+        private System.Windows.Forms.DataGridView dgvProductos;
+        private RecuperatorioDataSet recuperatorioDataSet;
+        private System.Windows.Forms.BindingSource productosBindingSource;
+        private RecuperatorioDataSetTableAdapters.ProductosTableAdapter productosTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descripcionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn precioDataGridViewTextBoxColumn;
+        private System.Windows.Forms.TextBox tbPrecio;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ PerezRodrigo/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PerezRodrigo
+{
+    public partial class Form1 : Form
+    {
+        public string connectionString = ConfigurationManager.ConnectionStrings["PerezRodrigo.Properties.Settings.cadena"].ConnectionString;
+
+        public class Producto
+        {
+            public string descripcion { get; set; }
+            public decimal precio { get; set; }
+            public override string ToString()
+            {
+                return descripcion;
+            }
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            tbId.Enabled = false;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'recuperatorioDataSet.Productos' Puede moverla o quitarla según sea necesario.
+            this.productosTableAdapter.Fill(this.recuperatorioDataSet.Productos);
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbDescripcion.Text) || string.IsNullOrWhiteSpace(tbPrecio.Text))
+            {
+                MessageBox.Show("Complete los datos necesarios", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                Producto producto = new Producto();
+                producto.descripcion = tbDescripcion.Text.Trim();
+                producto.precio = decimal.Parse(tbPrecio.Text);
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string sql = "INSERT INTO Productos (descripcion, precio) VALUES (@descripcion, @precio)";
+                    SqlCommand cmd = new SqlCommand(sql, connection);
+
+                    cmd.Parameters.AddWithValue("@descripcion", producto.descripcion);
+                    cmd.Parameters.AddWithValue("@precio", producto.precio);
+
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                }
+
+                tbDescripcion.Clear();
+                tbPrecio.Clear();
+                tbDescripcion.Focus();
+
+                this.productosTableAdapter.Fill(this.recuperatorioDataSet.Productos);
+
+                MessageBox.Show("Producto agregado exitosamente", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("El precio debe ser un número válido", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbId.Text))
+            {
+                MessageBox.Show("Seleccione un producto de la grilla para eliminar", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            DialogResult resultado = MessageBox.Show(
+                $"¿Está seguro que desea eliminar el producto '{tbDescripcion.Text}'?",
+                "Confirmar eliminación",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+
+            if (resultado == DialogResult.Yes)
+            {
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        string sql = "DELETE FROM Productos WHERE id = @id";
+                        SqlCommand cmd = new SqlCommand(sql, connection);
+                        cmd.Parameters.AddWithValue("@id", int.Parse(tbId.Text));
+
+                        connection.Open();
+                        int filasAfectadas = cmd.ExecuteNonQuery();
+
+                        if (filasAfectadas > 0)
+                        {
+                            MessageBox.Show("Producto eliminado exitosamente", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+                            btnLimpiar_Click(sender, e);
+
+
+                            this.productosTableAdapter.Fill(this.recuperatorioDataSet.Productos);
+                        }
+                    }
+
+                }
+                catch
+                {
+                    MessageBox.Show("Error al eliminar el producto", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+                }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvProductos.Rows[e.RowIndex];
+
+            
+                tbId.Text = row.Cells[0].Value?.ToString() ?? "";
+                tbDescripcion.Text = row.Cells[1].Value?.ToString() ?? "";
+                tbPrecio.Text = row.Cells[2].Value?.ToString() ?? "";
+            }
+        }
+
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+
+            tbId.Clear();
+            tbDescripcion.Clear();
+            tbPrecio.Clear();
+
+
+            tbDescripcion.Focus();
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbId.Text))
+            {
+                MessageBox.Show("Seleccione un producto de la grilla para modificar", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+
+            if (string.IsNullOrWhiteSpace(tbDescripcion.Text) || string.IsNullOrWhiteSpace(tbPrecio.Text))
+            {
+                MessageBox.Show("Complete todos los campos", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                decimal precio = decimal.Parse(tbPrecio.Text);
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string sql = "UPDATE Productos SET descripcion = @descripcion, precio = @precio WHERE id = @id";
+                    SqlCommand cmd = new SqlCommand(sql, connection);
+
+                    cmd.Parameters.AddWithValue("@id", int.Parse(tbId.Text));
+                    cmd.Parameters.AddWithValue("@descripcion", tbDescripcion.Text.Trim());
+                    cmd.Parameters.AddWithValue("@precio", precio);
+
+                    connection.Open();
+                    int filasAfectadas = cmd.ExecuteNonQuery();
+
+                    if (filasAfectadas > 0)
+                    {
+                        MessageBox.Show("Producto modificado exitosamente", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+                        btnLimpiar_Click(sender, e);
+
+
+                        this.productosTableAdapter.Fill(this.recuperatorioDataSet.Productos);
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se encontró el producto", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("El precio debe ser un número válido", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+          
+        }
+
+        private void btnFiltrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                if (string.IsNullOrWhiteSpace(tbDesde.Text) && string.IsNullOrWhiteSpace(tbHasta.Text))
+                {
+                    MessageBox.Show("Ingrese al menos un valor de precio (mínimo o máximo)", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                
+                string sql = "SELECT * FROM Productos WHERE 1=1";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = connection;
+
+                    
+                    if (!string.IsNullOrWhiteSpace(tbDesde.Text))
+                    {
+                        decimal precioMin = decimal.Parse(tbDesde.Text);
+                        sql += " AND precio >= @precioMin";
+                        cmd.Parameters.AddWithValue("@precioMin", precioMin);
+                    }
+
+                    
+                    if (!string.IsNullOrWhiteSpace(tbHasta.Text))
+                    {
+                        decimal precioMax = decimal.Parse(tbHasta.Text);
+                        sql += " AND precio <= @precioMax";
+                        cmd.Parameters.AddWithValue("@precioMax", precioMax);
+                    }
+
+                    cmd.CommandText = sql;
+                    connection.Open();
+
+                    
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    
+                    dgvProductos.DataSource = dt;
+
+                    
+                    if (dt.Rows.Count == 0)
+                    {
+                        MessageBox.Show("No se encontraron productos en ese rango de precios", "Sin resultados", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Los precios deben ser números válidos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        
+
+        private void btnDesfltrar_Click(object sender, EventArgs e)
+        {
+            tbDesde.Clear();
+            tbHasta.Clear();
+            this.productosTableAdapter.Fill(this.recuperatorioDataSet.Productos);
+        }
+    }
+
+    }
